81d290ca4b6a622b57e42a0d405551c9f4718e53
Pez Cuckow <email@pezcuckow.com>
2023-09-17 18:21:32 +0200

    :sparkles: Record how many messages each user has sent

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const qrcode = require('qrcode-terminal')
 
 const util = require('util')
 
+const { Log, logger } = require('debug-level')
+
+const log = new Log('whatsapp-community-bot')
+
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
@@ -12,7 +16,7 @@ const client = new Client({
   },
 })
 
-function log(label, object) {
+function detailedLog(label, object) {
   console.log(label, util.inspect(object, { depth: 3, colors: true }))
 }
 
@@ -40,14 +44,14 @@ client.on('qr', (qr) => {
 })
 
 client.on('ready', async () => {
-  console.log('Client is ready!')
+  log.debug('Client is ready!')
 
   const community = await client.getChatById(COMMUNITY_ID)
 
   const communityAdmins = community.participants.filter((user) => user.isAdmin)
 
-  console.info(`Grabbing all participants for "${community.name}"`)
-  console.info(`Loaded ${communityAdmins.length} community admins`)
+  log.info(`Grabbing all participants for "${community.name}"`)
+  log.info(`Loaded ${communityAdmins.length} community admins`)
 
   const allChats = await client.getChats()
 
@@ -57,7 +61,7 @@ client.on('ready', async () => {
       (chat) => chat.groupMetadata.parentGroup?._serialized === COMMUNITY_ID
     )
 
-  console.info(
+  log.info(
     `Loaded ${communityChats.length} chats that are part of the community`
   )
 
@@ -72,27 +76,28 @@ client.on('ready', async () => {
     const chatUsers = chat.participants.filter((user) => !user.isAdmin)
     const chatAdmins = chat.participants.filter((user) => user.isAdmin)
 
-    console.info(
+    log.info(
       `Chat ${chat.name} has ${chatUsers.length} normal users plus ${chatAdmins.length} group admin(s)`
     )
 
-    if (chatUsers.length) {
-      console.warn(`Chat ${chat.name} has no users, is the authenticated user a member of the group?`)
+    if (!chatUsers.length) {
+      log.warn(
+        `Chat ${chat.name} has no users, is the currently authenticated user a member of the group?`
+      )
     }
 
     for (const user of chat.participants) {
       const userId = user.id._serialized
       if (user.isAdmin) {
         admins.set(userId, user)
-        participants.delete(userId) // may have been previously added from another group
-      } else {
-        participants.set(user.id._serialized, user)
       }
+
+      participants.set(user.id._serialized, user)
     }
   }
 
-  console.info(
-    `Loaded a total of ${participants.size} users plus ${admins.size} admins (including ${communityAdmins.length} community admins)`
+  log.info(
+    `Loaded a total of ${participants.size} users including ${admins.size} admins (and ${communityAdmins.length} community admins)`
   )
 
   /*
@@ -102,8 +107,10 @@ client.on('ready', async () => {
     _serialized: '120363047641738769@g.us'
   },*/
 
+  const missingUsersMessages = new Map()
+
   for (const chat of sortedChats) {
-    console.info(
+    log.info(
       `Checking ${MESSAGES_TO_CONSIDER_RECENT} most recent messages from ${chat.name}`
     )
 
@@ -114,17 +121,20 @@ client.on('ready', async () => {
       TYPES_OF_MESSAGES_TO_COUNT.has(message.type)
     )
 
-    console.debug(
+    log.debug(
       `Filtered out ${
         recentMessages.length - recentMessagesToCount.length
       } non-countable messages`
     )
 
+    if (!recentMessagesToCount.length) {
+      log.warn(`Found no messages in ${chat.name} is the current authenticated user a member of this group?`)
+      continue;
+    }
+
     const messagesPerUserInThisChat = new Map()
     for (const message of recentMessagesToCount) {
       const userId = message.author
-
-      console.log(message)
 
       if (!userId) {
         // Skip system messages
@@ -141,8 +151,12 @@ client.on('ready', async () => {
       // All chats
       const user = participants.get(userId)
       if (!user) {
-        console.warn(`Message from unknown user ${userId}`)
-        log('message', message)
+        log.warn(`Message from unknown user ${userId}, user not found in community, check this number - "${message.body.slice(0, 100)}..."`)
+        if (!missingUsersMessages.has(userId)) {
+          missingUsersMessages.set(userId, [])
+        }
+
+        missingUsersMessages.get(userId).push(message)
       } else {
         if (!user.messages) {
           user.messages = [message]
@@ -152,119 +166,119 @@ client.on('ready', async () => {
       }
     }
 
-    log(messagesPerUserInThisChat.entries())
-
-    const mostActiveUsersInThisGroup = Array.from(
+    const mostActiveUsersInGroup = Array.from(
       messagesPerUserInThisChat.entries()
-    ).sort((a, b) => b[0].length - a[0].length)
+    )
+      .sort(
+        ([aUserId, aMessages], [bUserId, bMessages]) =>
+          bMessages.length - aMessages.length
+      )
+      .splice(0, NUMBER_OF_ACTIVE_USERS_TO_REPORT_PER_GROUP)
 
-    for (const [userId, messages] of mostActiveUsersInThisGroup.splice(
-      0,
-      NUMBER_OF_ACTIVE_USERS_TO_REPORT_PER_GROUP
-    )) {
-      console.info(
-        `User ${userId} has sent ${messages.length} messages (looked at last ${MESSAGES_TO_CONSIDER_RECENT})`
+    for (const [userId, messages] of mostActiveUsersInGroup) {
+      log.debug(
+        `User ${userId} has sent ${messages.length} messages`
       )
     }
   }
 })
 
 client.on('message', (msg) => {
-  console.log('Message received', msg)
+  log.debug('Message received', msg)
 })
 
 // All other
 
 client.on('auth_failure', (message) => {
-  console.log('Auth failure', message)
+  log.debug('Auth failure', message)
 })
 
 client.on('authenticated', () => {
-  console.log('authenticated')
+  log.debug('authenticated')
 })
 
 client.on('change_battery', (batteryInfo) => {
-  console.log('change_battery', batteryInfo)
+  log.debug('change_battery', batteryInfo)
 })
 
 client.on('change_state', (state) => {
-  console.log('change_state', state)
+  log.debug('change_state', state)
 })
 
 client.on('chat_archived', (chat, currentState, previousState) => {
-  console.log('chat_archived', chat, currentState, previousState)
+  log.debug('chat_archived', chat, currentState, previousState)
 })
 
 client.on('chat_removed', (chat) => {
-  console.log('chat_removed', chat)
+  log.debug('chat_removed', chat)
 })
 
 /* Noisy
 client.on('contact_changed', (message, oldId, newId, isContact) => {
-  console.log('contact_changed', message, oldId, newId, isContact)
+  log.debug('contact_changed', message, oldId, newId, isContact)
 })
 */
 
 client.on('disconnected', (reason) => {
-  console.log('disconnected', reason)
+  log.debug('disconnected', reason)
 })
 
 client.on('group_admin_changed', (notification) => {
-  console.log('group_admin_changed', notification)
+  log.debug('group_admin_changed', notification)
 })
 
 client.on('group_join', (notification) => {
-  console.log('group_join', notification)
+  log.debug('group_join', notification)
 })
 
 client.on('group_leave', (notification) => {
-  console.log('group_leave', notification)
+  log.debug('group_leave', notification)
 })
 
 client.on('group_update', (notification) => {
-  console.log('group_update', notification)
+  log.debug('group_update', notification)
 })
 
 client.on('incoming_call', (call) => {
-  console.log('incoming_call', cal)
+  log.debug('incoming_call', cal)
 })
 
 client.on('media_uploaded', (message) => {
-  console.log('media_uploaded', message)
+  log.debug('media_uploaded', message)
 })
 
 /* Noisy
 client.on('message_ack', (message, ack) => {
-  console.log('message_ack', message, ack)
+  log.debug('message_ack', message, ack)
 })
 */
 
 /* Noisy
 client.on('message_create', (message) => {
-  console.log('message_create', message)
+  log.debug('message_create', message)
 })
 */
 
 /* Noisy 
 client.on('message_edit', (message, newBody, oldBody) => {
-  console.log('message_edit', message, newBody, oldBody)
+  log.debug('message_edit', message, newBody, oldBody)
 })
 */
 
 /* Noisy
 client.on('message_reaction', (reaction) => {
-  console.log('message_reaction', reaction)
+  log.debug('message_reaction', reaction)
 })
 */
 
 client.on('message_revoke_everyone', (message, revoked_msg) => {
-  console.log('message_revoke_everyone', message, revoked_msg)
+  log.debug('message_revoke_everyone', message, revoked_msg)
 })
 
 client.on('message_revoke_me', (message) => {
-  console.log('message_revoke_me', message)
+  log.debug('message_revoke_me', message)
 })
 
 client.initialize()
 
-console.log('Client initialized')
+log.info('Client initializing')
